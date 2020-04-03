98a2a723b5dd6f83dd88c861e7cedf424d8cd701
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2020-04-03 13:02:36 -0400

    Updates

---
+++ precache-manifest.9d79c31df1a85be9122b82d530a9e84c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1525e22f3631125a3ccd5304ad7780f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b3a50b9f195fb027f92",
+    "url": "/static/css/main.a8095b7e.chunk.css"
+  },
+  {
+    "revision": "b42ff795eabe1bfbd3e8",
+    "url": "/static/js/2.b6bbb8a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b6bbb8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3a50b9f195fb027f92",
+    "url": "/static/js/main.0dde48ee.chunk.js"
+  },
+  {
+    "revision": "3bc54240a0f9a97ccb8a",
+    "url": "/static/js/runtime-main.6d686c10.js"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/static/media/spotify-icon-logo.11dc437a.png"
+  }
+]);
