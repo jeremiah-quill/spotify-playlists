504697dce5e61d31e8c878bf0eb50c27cb23427f
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2020-04-03 14:07:05 -0400

    Updates

---
+++ precache-manifest.db894252d995ae5793dbb6db3136c85f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1a5aa26f488c43898e186d1a392788",
+    "url": "/spotify-playlists/index.html"
+  },
+  {
+    "revision": "d3c3aacb84d019b58c94",
+    "url": "/spotify-playlists/static/css/main.a8095b7e.chunk.css"
+  },
+  {
+    "revision": "b42ff795eabe1bfbd3e8",
+    "url": "/spotify-playlists/static/js/2.b6bbb8a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-playlists/static/js/2.b6bbb8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c3aacb84d019b58c94",
+    "url": "/spotify-playlists/static/js/main.ba6afef9.chunk.js"
+  },
+  {
+    "revision": "4f1e2d7bcc4565212baf",
+    "url": "/spotify-playlists/static/js/runtime-main.54ecaaca.js"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/spotify-playlists/static/media/spotify-icon-logo.11dc437a.png"
+  }
+]);
